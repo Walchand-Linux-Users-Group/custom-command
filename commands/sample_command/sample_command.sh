a59235ebc21ea9204cf60e8acc852b05f941b0d1
Gawande Pranav Anil <135448154+PranavG1203@users.noreply.github.com>
2024-10-04 01:42:07 +0530

    Create sample_command.sh

---
+++ commands/sample_command/sample_command.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  # This line tells the system to use the Bash shell to interpret the script
+
+# Code body starts here
+# Place your commands here. 
+#For example, you might declare variables, execute commands, or call functions.
+
+# Code body ends here
